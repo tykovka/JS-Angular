32ece14460e9a81adf078c636b190fe6c7812207
Irina <irinabereza161@gmail.com>
2019-09-22 14:41:16 +0300

    балуюсь с классами, наследованием и переопределением методов

--- lesson9.js
+++ lesson9.js
@@ -44,16 +44,16 @@ class Hamburger {
 
   getStuffing() { return this.stuffing }
 
-  calculatePrice() { 
-    return this.size.price + 
-    this.stuffing.reduce((sum, item) => sum += item.price, 0) + 
-    this.toppings.reduce((sum, item) => sum += item.price, 0); 
+  calculatePrice() {
+    return this.size.price +
+      this.stuffing.reduce((sum, item) => sum += item.price, 0) +
+      this.toppings.reduce((sum, item) => sum += item.price, 0);
   }
 
-  calculateCalories() { 
-    return this.size.kkal + 
-    this.stuffing.reduce((sum, item) => sum +=  item.kkal, 0) + 
-    this.toppings.reduce((sum, item) => sum +=  item.kkal, 0); 
+  calculateCalories() {
+    return this.size.kkal +
+      this.stuffing.reduce((sum, item) => sum += item.kkal, 0) +
+      this.toppings.reduce((sum, item) => sum += item.kkal, 0);
   }
 }
 
@@ -86,9 +86,46 @@ const stuffings = {
     name: 'bacon',
     price: 50,
     kkal: 150
-  }
+  },
+  egg: {
+    name: 'egg',
+    price: 10,
+    kkal: 80
+  },
+  fries: {
+    name: 'fries',
+    price: 50,
+    kkal: 180
+  },
+  beef: {
+    name: 'beef',
+    price: 150,
+    kkal: 240
+  }, 
 }
 
+class Pro100Burger extends Hamburger {
+ constructor() {
+   super(sizes.small, [stuffings.cheese, stuffings.salat, stuffings.beef]);
+ }
+}
+
+class BreakfastBurger extends Hamburger {
+  constructor() {
+    super(sizes.small, [stuffings.egg, stuffings.beef, stuffings.cheese]);
+  }
+
+  addTopping(...toppings) { this.toppings.push(...toppings) }
+ }
+
+const breakfastBurger = new BreakfastBurger();
+breakfastBurger.addTopping(stuffings.cheese, stuffings.cheese, stuffings.cheese);
+console.log('breakfastBurger price ' + breakfastBurger.calculatePrice());
+
+const pro100Burger = new Pro100Burger();
+pro100Burger.addTopping(stuffings.cheese, stuffings.cheese);
+console.log(pro100Burger.calculateCalories());
+
+
 const burger = new Hamburger(sizes.big, [stuffings.cheese, stuffings.salat])
-burger.addTopping(stuffings.bacon);
-console.log(burger.calculateCalories())
+
