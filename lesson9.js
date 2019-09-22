037722fc009cb1ebb92472780ebd3b2bff98e7d0
Irina <irinabereza161@gmail.com>
2019-09-22 14:20:26 +0300

    task 2

--- lesson9.js
+++ lesson9.js
@@ -1,12 +1,12 @@
 class GoodsList {
-  constructor (title, count, cost) { //создание экземпляра класса
+  constructor(title, count, cost) { //создание экземпляра класса
     this.title = title;
     this.count = count;
     this.cost = cost;
   }
 
   renderCatalog() {
-  //отрисовать каталог
+    //отрисовать каталог
   }
 
   addToCart() {
@@ -15,7 +15,7 @@ class GoodsList {
 
   removeFromCart() {
     //удалить из корзины
-  } 
+  }
 
   updateCart() {
     //обновить и отрисовать корзину
@@ -28,12 +28,67 @@ class GoodsList {
 
 
 class Hamburger {
-  constructor(size, stuffing) { ... }
-  addTopping(topping) { // Добавить добавку }
-  removeTopping(topping) { // Убрать добавку }
-  getToppings(topping) { // Получить список добавок }
-  getSize() { // Узнать размер гамбургера }
-  getStuffing() { // Узнать начинку гамбургера }
-  calculatePrice() { // Узнать цену }
-  calculateCalories() { // Узнать калорийность }
+  constructor(size, stuffing) {
+    this.size = size;
+    this.stuffing = stuffing;
+    this.toppings = [];
   }
+
+  addTopping(topping) { this.toppings.push(topping) }
+
+  removeTopping(topping) { this.toppings = this.toppings.filter(item => item !== topping) }
+
+  getToppings() { return this.toppings }
+
+  getSize() { return this.size; }
+
+  getStuffing() { return this.stuffing }
+
+  calculatePrice() { 
+    return this.size.price + 
+    this.stuffing.reduce((sum, item) => sum += item.price, 0) + 
+    this.toppings.reduce((sum, item) => sum += item.price, 0); 
+  }
+
+  calculateCalories() { 
+    return this.size.kkal + 
+    this.stuffing.reduce((sum, item) => sum +=  item.kkal, 0) + 
+    this.toppings.reduce((sum, item) => sum +=  item.kkal, 0); 
+  }
+}
+
+
+const sizes = {
+  big: {
+    name: 'big',
+    price: 100,
+    kkal: 40
+  },
+  small: {
+    name: 'small',
+    price: 50,
+    kkal: 20
+  }
+};
+
+const stuffings = {
+  cheese: {
+    name: 'cheese',
+    price: 10,
+    kkal: 20
+  },
+  salat: {
+    name: 'salat',
+    price: 20,
+    kkal: 5
+  },
+  bacon: {
+    name: 'bacon',
+    price: 50,
+    kkal: 150
+  }
+}
+
+const burger = new Hamburger(sizes.big, [stuffings.cheese, stuffings.salat])
+burger.addTopping(stuffings.bacon);
+console.log(burger.calculateCalories())
